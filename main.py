ce8b3a55df390dd446959ae5a4487347487e5b99
sm7177 <subrinamaharjan2020@gmail.com>
2024-12-04 18:44:09 +0545

    .

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#tail recursion
+
 # def tail(n):
 #     if(n!=0):
 #         print(n)
@@ -5,3 +7,52 @@
         
 # x=4
 # tail(x)
+
+
+
+
+#head recursion
+
+# def head(n):
+#     if(n!=0):
+#         head(n-1)
+#         print(n)
+        
+# x=4
+# head(x)
+
+
+
+
+
+# #tree recursion
+# def tree(n):
+#     if(n!=0):
+#         print(n)
+#         tree(n-1)
+#         tree(n-1)
+        
+# x=2
+# tree(x)
+
+
+
+
+
+
+#indirect recursion
+def funA(n):
+    if(n>0):
+        print("",n,end='')
+        funB(n-1)
+        
+def funB(n):
+    if(n>1):
+        print("",n,end='')
+        
+        funA(n//2)
+        
+funA(20)
+              
+              
+        
